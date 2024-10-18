19cb01dafb8f7d1fe1d02d907f362222fbaea5e2
justinparesu <t236009@st.yamagata-u.ac.jp>
2024-10-18 19:30:02 +0900

    Add files via upload

---
+++ scatman.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int k;
+    printf("How many students?");
+    scanf("%d" , &k);
+    
+    printf("The numbr of students is %d" , k);
+    
+    return 0;
+    
+
+}
