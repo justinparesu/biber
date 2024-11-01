28f04faf3b33ea86e9a80250ab0f204c2bf3caa8
justinparesu <t236009@st.yamagata-u.ac.jp>
2024-11-01 19:30:45 +0900

    Create 1101.c

---
+++ 1101.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<limits.h>
+#include<float.h>
+
+int main(void)
+{
+    printf("char absolute maximum:%d\n", UCHAR_MAX);
+    printf("char maximum:%d\n" , CHAR_MAX);
+    printf("char minimum:%d\n" , CHAR_MIN);
+    
+    return 0;
+}
